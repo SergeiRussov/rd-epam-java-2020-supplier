21d82ae3892b64556e1a20eb38c0a1d2b2cd44a1
StarBun1379 <41346424+StarBun1379@users.noreply.github.com>
2020-04-14 22:30:50 +0400

    Create project structure

---
+++ ms-supplier-impl/src/main/java/com/epam/rd/SupplierApplication.java
@@ -0,0 +1,10 @@
+package com.epam.rd;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SupplierApplication {
+    public static void main(String[] args) {
+        log.info("Hello world!");
+    }
+}
