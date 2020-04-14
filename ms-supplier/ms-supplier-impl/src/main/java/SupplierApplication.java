53e360cedf4f1ffe2611548dca6ea61ecb9e1497
Olgatltt <olgatltt@rambler.ru>
2020-04-14 23:50:45 +0400

    Creating the app structure

---
+++ ms-supplier/ms-supplier-impl/src/main/java/SupplierApplication.java
@@ -0,0 +1,9 @@
+import java.util.logging.Logger;
+
+public class SupplierApplication {
+
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(String.valueOf(SupplierApplication.class));
+        logger.info("Hello");
+    }
+}
