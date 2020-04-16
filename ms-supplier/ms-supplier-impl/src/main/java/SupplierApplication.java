ffcf87daab3b79d05f1f4d9aa591d3816e1ffb84
Olgatltt <olgatltt@rambler.ru>
2020-04-16 21:49:03 +0400

    Creating the app structure

--- ms-supplier/ms-supplier-impl/src/main/java/SupplierApplication.java
+++ ms-supplier/ms-supplier-impl/src/main/java/SupplierApplication.java
@@ -3,6 +3,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SupplierApplication {
     public static void main(String[] args) {
-       // log.info("Hello world!");
+        log.info("Hello");
     }
 }
