2e93395b9b43da26944426aa84fbf595d3b8f4e5
ajrat <tlt_asm@mail.ru>
2020-04-30 12:55:16 +0400

    #35 Реализовать сущности Order и репозиторий для работы с ней v.1

--- ms-supplier-impl/src/main/java/com/epam/rd/domain/Order.java
+++ ms-supplier-impl/src/main/java/com/epam/rd/domain/Order.java
@@ -1,5 +1,33 @@
 package com.epam.rd.domain;
 
-public class Order {
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
+public class Order {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    UUID id;
+
+    @Column(name = "customer")
+    private String customer;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "amount")
+    private long amount;
+
+    @Column(name = "creation_date")
+    private Date creationDate;
+
+    @Column(name = "update_date")
+    private Date updateDate;
 }
