89a0fc921c4b5734b3ffe9789a87a8c397a5a0c4
ajrat <tlt_asm@mail.ru>
2020-05-01 02:43:31 +0400

    #35 Реализовать сущности Order и репозиторий для работы с ней v.2

--- ms-supplier-impl/src/main/java/com/epam/rd/domain/Order.java
+++ ms-supplier-impl/src/main/java/com/epam/rd/domain/Order.java
@@ -2,19 +2,17 @@ package com.epam.rd.domain;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import java.util.Date;
+import javax.persistence.*;
+import java.time.ZonedDateTime;
 import java.util.UUID;
 
 @Entity
+@Table(name = "supplier.order")
 @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
 public class Order {
     @Id
     @GeneratedValue(generator = "uuid")
-    UUID id;
+    private UUID id;
 
     @Column(name = "customer")
     private String customer;
@@ -26,8 +24,67 @@ public class Order {
     private long amount;
 
     @Column(name = "creation_date")
-    private Date creationDate;
+    private ZonedDateTime creationDate;
 
     @Column(name = "update_date")
-    private Date updateDate;
+    private ZonedDateTime updateDate;
+
+    public Order() {
+    }
+
+    public Order(String customer, String status, long amount, ZonedDateTime creationDate) {
+        this.customer = customer;
+        this.status = status;
+        this.amount = amount;
+        this.creationDate = creationDate;
+    }
+
+    public Order(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+
+    public void setUpdateDate(ZonedDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", customer='" + customer + '\'' +
+                ", status='" + status + '\'' +
+                ", amount=" + amount +
+                ", creationDate=" + creationDate +
+                ", updateDate=" + updateDate +
+                '}';
+    }
 }
