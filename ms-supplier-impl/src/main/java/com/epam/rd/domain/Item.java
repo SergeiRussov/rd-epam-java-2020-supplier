3be8b2b4ac524752b09b1042f026bb5220915289
StarBun1379 <41346424+StarBun1379@users.noreply.github.com>
2020-04-30 19:07:10 +0400

    Domain Item entity and repository to work with

--- ms-supplier-impl/src/main/java/com/epam/rd/domain/Item.java
+++ ms-supplier-impl/src/main/java/com/epam/rd/domain/Item.java
@@ -1,4 +1,44 @@
 package com.epam.rd.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+import javax.persistence.ManyToOne;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+/**
+ * POJO class {@code Item} represents domain entity from item table
+ *
+ * @author Aminev Ramil
+ */
+
+@Entity
+@Table(name = "item")
+@Data
+@NoArgsConstructor
 public class Item {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id")
+    private Product productId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order orderId;
+
+    private OffsetDateTime creationDate;
+    private OffsetDateTime updateDate;
 }
