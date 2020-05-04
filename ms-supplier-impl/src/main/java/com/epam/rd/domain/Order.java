fd26fbc4580a27e43f877ddf812b50b7b81c2974
AiratSalakhov <61519443+AiratSalakhov@users.noreply.github.com>
2020-05-04 19:38:01 +0400

    #35 fix2 (#46)
    Co-authored-by: ajrat <tlt_asm@mail.ru>

--- ms-supplier-impl/src/main/java/com/epam/rd/domain/Order.java
+++ ms-supplier-impl/src/main/java/com/epam/rd/domain/Order.java
@@ -1,4 +1,49 @@
 package com.epam.rd.domain;
 
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+/**
+ * POJO class {@code Order} represents domain entity from order table
+ *
+ * @author Salakhov Airat
+ */
+
+@Data
+@Entity
+@Table(name = "supplier.order")
+@GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
 public class Order {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    private UUID id;
+
+    @Column(name = "customer")
+    private String customer;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "amount")
+    private long amount;
+
+    @Column(name = "creation_date")
+    private ZonedDateTime creationDate;
+
+    @Column(name = "update_date")
+    private ZonedDateTime updateDate;
+
+    public Order() {
+    }
+
+    public Order(String customer, String status, long amount, ZonedDateTime creationDate) {
+        this.customer = customer;
+        this.status = status;
+        this.amount = amount;
+        this.creationDate = creationDate;
+    }
 }
---
+++ ms-supplier-impl/src/main/java/com/epam/rd/repository/OrderRepository.java
@@ -0,0 +1,80 @@
+package com.epam.rd.repository;
+
+import com.epam.rd.domain.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.HibernateException;
+import org.hibernate.Transaction;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+public class OrderRepository implements Repository<Order> {
+    private static final EntityManager entityManager =
+            Persistence.createEntityManagerFactory("supplier-pu").createEntityManager();
+
+    @Override
+    public Optional<Order> findById(UUID id) {
+
+        log.info("=== FINDING id={}", id);
+        try {
+            return Optional.ofNullable(entityManager.find(Order.class, id));
+        } catch (HibernateException e) {
+            log.info(e.getMessage());
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Order> findAll() {
+        Transaction tx = null;
+        List<Order> orderList = new ArrayList<Order>();
+
+        try {
+            orderList = entityManager.createQuery("FROM Order", Order.class).getResultList();
+        } catch (HibernateException e) {
+            log.info(e.getMessage());
+        }
+        log.info("=== FIND ALL ORDERS...");
+        return orderList;
+    }
+
+    @Override
+    public void save(Order entity) {
+        EntityTransaction tx = null;
+
+        try {
+            tx = entityManager.getTransaction();
+            tx.begin();
+            entityManager.persist(entity);
+            entityManager.flush();
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            log.info(e.getMessage());
+        }
+        log.info("=== SAVED entity={}", entity);
+    }
+
+    @Override
+    public void delete(Order entity) {
+        EntityTransaction tx = null;
+
+        try {
+            tx = entityManager.getTransaction();
+            tx.begin();
+            entityManager.remove(entity);
+            entityManager.flush();
+            tx.commit();
+        } catch (HibernateException e) {
+            if (tx != null) tx.rollback();
+            log.info(e.getMessage());
+        }
+        log.info("=== DELETED entity={}", entity);
+    }
+}
