89076ed210fb780218fca03cfccaec9e7564f0ee
Olgatltt <olgatltt@rambler.ru>
2020-05-04 18:31:30 +0400

    issue-33 ProductRepository, исправлены ошибки

--- ms-supplier-impl/src/main/java/com/epam/rd/repository/ProductRepository.java
+++ ms-supplier-impl/src/main/java/com/epam/rd/repository/ProductRepository.java
@@ -1,61 +1,78 @@
 package com.epam.rd.repository;
 
+
 import com.epam.rd.domain.Product;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Slf4j
-public class ProductRepository {
+public class ProductRepository implements Repository<Product>{
     private static final String EXCEPTION = "Произошло исключение при обработке команды ";
     private EntityManagerFactory emf = Persistence.createEntityManagerFactory("supplier-pu");
     private EntityManager manager = emf.createEntityManager();
 
-    public ProductRepository() {
-    }
-
-    public Product findById(UUID id) {
+    @Override
+    public Optional<Product> findById(UUID id) {
+        log.info("Finding Product entity by id={}", id);
         try {
-            Product product = manager.find(Product.class, id);
-            return product;
-        } catch (CommandProcessingException e) {
+            return Optional.ofNullable(manager.find(Product.class, id));
+        } catch (PersistenceException e) {
             log.debug(EXCEPTION, e);
         }
-        return null;
+        return Optional.empty();
     }
 
-    public void delete(UUID id) {
-        Product product = manager.find(Product.class, id);
-        manager.getTransaction().begin();
+    @Override
+    public List<Product> findAll() {
+        log.info("Finding all Product entities in repository");
         try {
-            manager.remove(product);
-        } catch (CommandProcessingException e) {
+            Query findAllQuery = manager.createNativeQuery("select * from item;", Product.class);
+            return findAllQuery.getResultList();
+        } catch (PersistenceException e) {
             log.debug(EXCEPTION, e);
         }
-        manager.getTransaction().commit();
+        return Collections.emptyList();
     }
 
-    public void updateName(UUID id, String name) {
-        Product product = manager.find(Product.class, id);
-        manager.getTransaction().begin();
-        try {
-            product.setName(name);
-        } catch (CommandProcessingException e) {
-            log.debug(EXCEPTION, e);
+    public void delete(Product product) {
+
+        if (manager.find(Product.class, product.getId()) != null) {
+            manager.getTransaction().begin();
+            try {
+                manager.remove(product);
+            } catch (PersistenceException e) {
+                log.debug(EXCEPTION, e);
+            }
+            manager.getTransaction().commit();
+        } else {
+            log.info("Entity is not presented in repository: {}", product);
         }
-        manager.getTransaction().commit();
+    }
+    @Override
+    public void save(Product product) {
+            try {
+                manager.getTransaction().begin();
+                if (manager.find(Product.class, product.getId()) != null) {
+                    log.info("Updating entity in repository. Updated Product: {}", product);
+                    manager.merge(product);
+                } else {
+                    log.info("Inserting entity to repository. New Product: {}", product);
+                    manager.persist(product);
+                }
+                manager.flush();
+                manager.getTransaction().commit();
+            } catch (javax.persistence.PersistenceException e) {
+                log.debug(EXCEPTION, e);
+            }
+        }
     }
 
-    public void insert(Product product) {
-        manager.getTransaction().begin();
-        try {
-            manager.persist(product);
-        } catch (CommandProcessingException e) {
-            log.debug(EXCEPTION, e);
-        }
-        manager.getTransaction().commit();
-    }
-}
+
