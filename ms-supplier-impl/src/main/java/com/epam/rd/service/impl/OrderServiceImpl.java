843a7da9ff8ffbb8b093b8af5b0ea3f91f1593b4
ajrat <tlt_asm@mail.ru>
2020-05-08 13:38:01 +0400

    #43 Необходимо реализовать сервис для работы с сущностью Order и протестировать его v.2

--- ms-supplier-impl/src/main/java/com/epam/rd/service/impl/OrderServiceImpl.java
+++ ms-supplier-impl/src/main/java/com/epam/rd/service/impl/OrderServiceImpl.java
@@ -1,15 +1,15 @@
 package com.epam.rd.service.impl;
 
-import com.epam.rd.domain.Item;
 import com.epam.rd.domain.Order;
-import com.epam.rd.repository.ItemRepository;
+import com.epam.rd.domain.OrderItem;
+import com.epam.rd.repository.OrderItemRepository;
 import com.epam.rd.repository.OrderRepository;
 import com.epam.rd.repository.ProductRepository;
 import com.epam.rd.service.OrderService;
 
+import java.time.OffsetDateTime;
 import java.time.ZonedDateTime;
 import java.util.Map;
-import java.util.Optional;
 import java.util.UUID;
 
 public class OrderServiceImpl implements OrderService {
@@ -25,11 +25,11 @@ public class OrderServiceImpl implements OrderService {
         orderRepository.save(newOrder);
         for (UUID key : order.keySet()) {
             OrderItem orderItem = new OrderItem();
-            orderItem.setCreationDate(ZonedDateTime.now());
-            orderItem.setUpdateDate(ZonedDateTime.now());
+            orderItem.setCreationDate(OffsetDateTime.now());
+            orderItem.setUpdateDate(OffsetDateTime.now());
             orderItem.setOrder(newOrder);
-            orderItem.setProduct(productRepository.findById(key));
-            orderItem.setQuantity(order.get(key));
+            orderItem.setProduct(productRepository.findById(key).get());
+            orderItem.setAmount(order.get(key));
             orderItemRepository.save(orderItem);
         }
         PaymentServiceImpl paymentService = new PaymentServiceImpl();
@@ -39,9 +39,8 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public void markAsPaid(UUID uuid) {
         OrderRepository orderRepository = new OrderRepository();
-        Optional order = orderRepository.findById(uuid);
-        Order newOrder = (Order) order.get();
-        newOrder.setStatus("paid");
-        orderRepository.save(newOrder);
+        Order order = orderRepository.findById(uuid).get();
+        order.setStatus("paid");
+        orderRepository.save(order);
     }
 }
