edf4094329abd47826894dbc6f1a53a4764c248f
ajrat <tlt_asm@mail.ru>
2020-05-11 10:19:20 +0400

    #43 Необходимо реализовать сервис для работы с сущностью Order и протестировать его v.9 after rebasing develop

--- ms-supplier-impl/src/test/java/com/epam/rd/service/impl/PaymentServiceImplTest.java
+++ ms-supplier-impl/src/test/java/com/epam/rd/service/impl/PaymentServiceImplTest.java
@@ -16,7 +16,8 @@ import java.time.OffsetDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
